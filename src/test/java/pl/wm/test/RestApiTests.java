7341bab533d053266894c3c343e7bca3c5b4d6cc
wlodarczakm <m.wlodarczak.it@protonmail.com>
2023-10-31 19:02:45 +0100

    endpoint changed to users/3

--- src/test/java/pl/wm/test/RestApiTests.java
+++ src/test/java/pl/wm/test/RestApiTests.java
@@ -11,7 +11,7 @@ public class RestApiTests {
     public void serverShouldReturn200() {
         given()
                 .when()
-                .get("https://reqres.in/api/users")
+                .get("https://reqres.in/api/users/3")
                 .then()
                 .statusCode(200);
     }
