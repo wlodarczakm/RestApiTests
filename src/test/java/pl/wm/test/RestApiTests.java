abbde1dd61576f666a6e738a6195c609a84efac1
wlodarczakm <m.wlodarczak.it@protonmail.com>
2023-10-31 19:13:39 +0100

    +simple test

--- src/test/java/pl/wm/test/RestApiTests.java
+++ src/test/java/pl/wm/test/RestApiTests.java
@@ -4,15 +4,27 @@ package pl.wm.test;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class RestApiTests {
 
     @Test
-    public void serverShouldReturn200() {
+    public void serverShouldReturn_200() {
         given()
                 .when()
                 .get("https://reqres.in/api/users/3")
                 .then()
                 .statusCode(200);
+    }
+
+    @Test
+    public void lastNameShouldBe_Wong(){
+        given()
+                .baseUri("https://reqres.in/api")
+                .pathParam("userId", 3)
+                .when()
+                .get("/users/{userId}")
+                .then()
+                .body("data.last_name", equalTo("Wong"));
     }
 }
