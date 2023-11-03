feae75a1bcd9c0613946d54e524848a8becef51d
wlodarczakm <m.wlodarczak.it@protonmail.com>
2023-11-03 18:02:56 +0100

    new tests

--- src/test/java/pl/wm/test/RestApiTests.java
+++ src/test/java/pl/wm/test/RestApiTests.java
@@ -1,10 +1,12 @@
 package pl.wm.test;
 
 
+import io.restassured.config.EncoderConfig;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.config;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 public class RestApiTests {
 
@@ -27,4 +29,43 @@ public class RestApiTests {
                 .then()
                 .body("data.last_name", equalTo("Wong"));
     }
-}
+
+    @Test
+    public void verifyHeadersAreSent(){
+
+         final String HEADER_VALUE = "someValue";
+
+        given()
+                .config(config().encoderConfig(EncoderConfig.encoderConfig()
+                        .appendDefaultContentCharsetToContentTypeIfUndefined(false)))
+                .baseUri("https://postman-echo.com")
+                .header("myheader", HEADER_VALUE)
+                .when()
+                .log().all()
+                .post("/post")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("headers.myheader", equalTo(HEADER_VALUE));
+    }
+
+    @Test
+    public void verifyResponseContentTypeAndCharset() {
+
+
+        given()
+                .config(config().encoderConfig(EncoderConfig.encoderConfig()
+                        .appendDefaultContentCharsetToContentTypeIfUndefined(false)))
+                .baseUri("https://postman-echo.com")
+                .when()
+                .log().all()
+                .post("/post")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .contentType("application/json")
+                .and().header("Content-Type", matchesPattern(".*charset=utf-8.*"));
+    }
+
+    }
+
