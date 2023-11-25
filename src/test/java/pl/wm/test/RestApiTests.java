6defce9e4be19f529b942883caf795b17447bb08
wlodarczakm <m.wlodarczak.it@protonmail.com>
2023-11-25 14:23:28 +0100

    new test

--- src/test/java/pl/wm/test/RestApiTests.java
+++ src/test/java/pl/wm/test/RestApiTests.java
@@ -1,11 +1,16 @@
 package pl.wm.test;
 
 
+import io.restassured.RestAssured;
 import io.restassured.config.EncoderConfig;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static io.restassured.RestAssured.config;
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class RestApiTests {
@@ -65,6 +70,34 @@ public class RestApiTests {
                 .statusCode(200)
                 .contentType("application/json")
                 .and().header("Content-Type", matchesPattern(".*charset=utf-8.*"));
+    }
+
+    @Test
+    public void verifySumOfResponseObjects(){
+
+        RestAssured.baseURI = "https://fakerestapi.azurewebsites.net/api/v1";
+        RequestSpecification httpRequest = RestAssured.given();
+        Response response = httpRequest.get("/Authors");
+
+        ResponseBody rBody = response.getBody();
+
+        String keyWord = "idBook";
+
+        int counter = 0;
+
+        int position = rBody.asString().indexOf(keyWord);
+        while (position != -1) {
+            counter++;
+            position = rBody.asString().indexOf(keyWord, position + 1);
+        }
+        System.out.println(counter);
+
+        String jsonResponse = response.getBody().asString();
+        JsonPath jsonPath = new JsonPath(jsonResponse);
+        int numberOfResponseObjects = jsonPath.getList("collection").size();
+        System.out.println(numberOfResponseObjects);
+
+        Assert.assertEquals(counter, numberOfResponseObjects, "OK");
     }
 }
 
